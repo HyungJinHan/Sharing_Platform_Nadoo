63a258037f3725fd83164e5d02e20375ed000574
HyungJinHan <han1210_36@naver.com>
2022-12-27 18:02:12 +0900

    2022.12.27 (한형진 front) 가이드 이미지 클릭 시, 가이드 슬라이드로 이동

--- nadoo/src/main/frontend/src/components/Test/Test.jsx
+++ nadoo/src/main/frontend/src/components/Test/Test.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import NavigatorMain from '../Navigator/NavigatorMain';
@@ -17,6 +17,11 @@ const Background = styled.div`
 `
 
 function Test() {
+  const homeRef = useRef();
+
+  const onHomeClick = () => {
+    homeRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <>
@@ -39,14 +44,138 @@ function Test() {
             className='Test_guide'
             alt='undefind'
             src={guideImg}
+            onClick={onHomeClick}
           />
           <br />
           <br />
           <br />
           <br />
+          <div className='Test_iconDiv'>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={day}
+              />
+              <br />
+              <p className='Test_iconText'>나두 일정</p>
+            </div>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={recycle}
+              />
+              <br />
+              <p className='Test_iconText'>나두 교환</p>
+            </div>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={together}
+              />
+              <br />
+              <p className='Test_iconText'>나두 함께</p>
+            </div>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={mega}
+              />
+              <br />
+              <p className='Test_iconText'>나두 공지</p>
+            </div>
+          </div>
+
           <br />
-          <Slider />
-          <br /><br />
+          <br />
+
+          <div className='Test_iconDiv'>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={day}
+              />
+              <br />
+              <p className='Test_iconText'>나두 일정</p>
+            </div>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={recycle}
+              />
+              <br />
+              <p className='Test_iconText'>나두 교환</p>
+            </div>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={together}
+              />
+              <br />
+              <p className='Test_iconText'>나두 함께</p>
+            </div>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={mega}
+              />
+              <br />
+              <p className='Test_iconText'>나두 공지</p>
+            </div>
+          </div>
+
+          <br />
+          <br />
+
+          <div className='Test_iconDiv'>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={day}
+              />
+              <br />
+              <p className='Test_iconText'>나두 일정</p>
+            </div>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={recycle}
+              />
+              <br />
+              <p className='Test_iconText'>나두 교환</p>
+            </div>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={together}
+              />
+              <br />
+              <p className='Test_iconText'>나두 함께</p>
+            </div>
+            <div className='Test_iconIfo'>
+              <img
+                className='Test_icon'
+                alt='undefind'
+                src={mega}
+              />
+              <br />
+              <p className='Test_iconText'>나두 공지</p>
+            </div>
+          </div>
+
+          <br />
+          <br />
+
           <div className='Test_iconDiv'>
             <div className='Test_iconIfo'>
               <img
@@ -87,6 +216,12 @@ function Test() {
           </div>
           <br />
           <br />
+          <br />
+          <div
+            ref={homeRef}
+          >
+            <Slider />
+          </div>
         </div>
       </Background>
       <NavigatorMain />
