9d91e420f5904726f119cab904cf4b8fa7be741b
Yoonkie96 <89620307+minyoongi96@users.noreply.github.com>
2022-12-27 15:57:20 +0900

    test

--- nadoo/src/main/java/com/nanum/nadoo/Service/NadooService.java
+++ nadoo/src/main/java/com/nanum/nadoo/Service/NadooService.java
@@ -1,10 +1,18 @@
 package com.nanum.nadoo.Service;
 
 import com.nanum.nadoo.Entity.Users;
+import com.nanum.nadoo.Repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface NadooService {
-    List<Users> getUsers();
+@Service
+public class NadooService{
+
+    @Autowired
+    UsersRepository uRespository;
+    public List<Users> getUsers() {
+        return uRespository.findAll();
+    }
 }
