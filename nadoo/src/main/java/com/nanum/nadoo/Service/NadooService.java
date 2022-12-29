42a745ad0bc098f60c00c20f5621d6b586b29623
Yoonkie96 <89620307+minyoongi96@users.noreply.github.com>
2022-12-29 17:03:50 +0900

    2022-12-29 17:00 (민윤기-back) 최신 나두, 종료 임박 unixtime값 추가 및 갯수 15개 limit, cross origin 적용 완료

--- nadoo/src/main/java/com/nanum/nadoo/Service/NadooService.java
+++ nadoo/src/main/java/com/nanum/nadoo/Service/NadooService.java
@@ -1,25 +1,8 @@
 package com.nanum.nadoo.Service;
 
-import com.nanum.nadoo.Entity.Trade;
-import com.nanum.nadoo.Repository.TradeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Map;
 
-import java.util.List;
-
-@Service
-public class NadooService{
-
-    @Autowired
-    TradeRepository tRepository;
-
-    public List<Trade> getRecentTrades() {
-        List<Trade> list = tRepository.findAllByOrderByTradeStarttimeDesc();
-        return list;
-    }
-
-    public List<Trade> getCloserTrades(){
-        List<Trade> list = tRepository.findAllByOrderByTradeEndtimeAsc();
-        return list;
-    }
+public interface NadooService {
+    public Map<String, Object> getRecentTrades();
+    public Map<String, Object> getCloserTrades();
 }
