fbfa6c01ef7edd43b1aa388a75626bf62bd02396
HyungJinHan <han1210_36@naver.com>
2022-12-30 10:38:58 +0900

    .

--- nadoo/src/main/frontend/src/components/GroupDetail/GroupDetail.jsx
+++ nadoo/src/main/frontend/src/components/GroupDetail/GroupDetail.jsx
@@ -46,48 +46,57 @@ function GroupDetail({
 
   return (
     <>
-      <NavigatorTopDetail groupTitle={groupTitle} />
+      <NavigatorTop />
       <Outlet />
-      <div className='GroupDetail_mapDiv'>
-        <img
-          className='GroupDetail_mapImg'
-          src={mapImg}
-          alt="undefind"
-        />
-      </div>
-      <br />
-      <Descriptions bordered>
-        <Descriptions.Item label="판매 물품">폼클렌징</Descriptions.Item>
-        <Descriptions.Item label="주소">Prepaid</Descriptions.Item>
-        <Descriptions.Item label="거래 인원">YES</Descriptions.Item>
-        <Descriptions.Item label="주최자">나는한형진</Descriptions.Item>
-        <Descriptions.Item label="Status">
-          {
-            toggleButton === true ?
-              <Badge status="error" text="거래 중지" />
-              :
-              <Badge status="processing" text="거래 허용" />
-          }
-          &nbsp;
-          &nbsp;
-          &nbsp;
-          <Space wrap>
-            <Button
-              type="dashed"
-              onClick={
-                () => {
-                  setToggleButton(!toggleButton);
+      {
+        (item) => (
+          <>
+            <div className='GroupDetail_mapDiv'>
+              <img
+                className='GroupDetail_mapImg'
+                src={mapImg}
+                alt="undefind"
+              />
+            </div>
+            <br />
+            <Descriptions bordered>
+              <Descriptions.Item label="판매 물품">item</Descriptions.Item>
+              <Descriptions.Item label="주소">Prepaid</Descriptions.Item>
+              <Descriptions.Item label="거래 인원">YES</Descriptions.Item>
+              <Descriptions.Item label="주최자">나는한형진</Descriptions.Item>
+              <Descriptions.Item label="Status">
+                {
+                  toggleButton === true ?
+                    <Badge status="error" text="거래 중지" />
+                    :
+                    <Badge status="processing" text="거래 허용" />
                 }
-              }
-            >
-              거래 상태 변경
-            </Button>
-          </Space>
-        </Descriptions.Item>
-        <Descriptions.Item label="상세 내용">
-          Y마트에서 양파 1망 1키로 기준으로 파는데 같이 사실분 구해요ㅠㅠㅠㅠㅠ
-        </Descriptions.Item>
-      </Descriptions>
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                <Space wrap>
+                  <Button
+                    type="dashed"
+                    onClick={
+                      () => {
+                        setToggleButton(!toggleButton);
+                      }
+                    }
+                  >
+                    거래 상태 변경
+                  </Button>
+                </Space>
+              </Descriptions.Item>
+              <Descriptions.Item label="상세 내용">
+                Y마트에서 양파 1망 1키로 기준으로 파는데 같이 사실분 구해요ㅠㅠㅠㅠㅠ
+              </Descriptions.Item>
+            </Descriptions>
+          </>
+        )
+      }
+
+
+
       <NavigatorMain />
       <Outlet />
     </>
