73311cc6996b4bf4768c0a82229b064356395736
Yoonkie96 <89620307+minyoongi96@users.noreply.github.com>
2023-01-04 11:21:32 +0900

    민윤기 .

--- nadoo/src/main/java/com/nanum/nadoo/Controller/NadooController.java
+++ nadoo/src/main/java/com/nanum/nadoo/Controller/NadooController.java
@@ -2,12 +2,15 @@ package com.nanum.nadoo.Controller;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.nanum.nadoo.Entity.KakaoVO;
+import com.nanum.nadoo.Service.LoginService;
 import com.nanum.nadoo.Service.NadooService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.log4j.Log4j2;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @CrossOrigin(origins="*")
@@ -17,6 +20,9 @@ public class NadooController {
 
   @Autowired
   NadooService service;
+
+  @Autowired
+  LoginService loginService;
 
   @RequestMapping("/nadoo")
   public String getAllUsers() {
@@ -59,5 +65,27 @@ public class NadooController {
     return gson.toJson(result);
   }
 
+  // 카카오 로그인
+  // 프론트에서 인가코드를 받아옴, 받은 인가코드로 카카오서버에서 액세스 토큰 받아와서 반환
+  @RequestMapping("/oauth/login")
+  public String login(@RequestParam(value = "code", required = false) String code) throws Exception {
+//    System.out.println("#######" + code);
+    String access_token = loginService.getAccessToken(code);
+//    System.out.println("###access_token###" + access_token);
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    return access_token;
+  }
+
+  // 받은 액세스 토큰으로 유저 정보 확인, 로그인/회원가입 처리
+  @RequestMapping("/oauth/userInfo")
+  public String userInfo(@RequestParam(value = "token") String token) throws Exception {
+    KakaoVO userInfo = loginService.getUserInfo(token);
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    Map<String, Object> map = new HashMap<String, Object>();
+
+    map.put("userInfo", userInfo);
+    return gson.toJson(map);
+  }
 
 }
