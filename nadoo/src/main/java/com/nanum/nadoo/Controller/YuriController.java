0640ca0a5bc1acebc669a6c8cefd9a72c2dcff13
kimyuri <dsuis1310009@naver.com>
2023-01-04 16:36:16 +0900

    유리컨트롤러

---
+++ nadoo/src/main/java/com/nanum/nadoo/Controller/YuriController.java
@@ -0,0 +1,38 @@
+package com.nanum.nadoo.Controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.nanum.nadoo.Dto.TradeDetailDTO;
+import com.nanum.nadoo.Service.NadooService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@Log4j2
+@CrossOrigin(origins = "*")
+public class SujinController {
+
+  @Autowired
+  NadooService service;
+
+//  @RequestMapping(value = "/nadoo/detail/{tradeIdx}", method = RequestMethod.GET)
+//  public String getDetail(@PathVariable Long tradeIdx) {
+//    TradeDetailDTO trade = service.getDetail(tradeIdx);
+//    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//
+//    return gson.toJson(trade);
+//  }
+
+  // 윤기
+//  @RequestMapping(value = "/nadoo/detail", method = RequestMethod.POST)
+  @RequestMapping(value = "/nadoo/detail")
+  public String getDetail2(@RequestBody Map<String, Long> paramMap) {
+    TradeDetailDTO trade = service.getDetail(paramMap.get("tradeIdx"));
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    return gson.toJson(trade);
+  }
+}
